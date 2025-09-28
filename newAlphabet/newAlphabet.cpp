def41c68d1f5e6629e0e94ed8ed0423e93a58f2e
kyletsubaki <91851573+kyletsubaki@users.noreply.github.com>
2025-09-28 16:14:11 -0700

    blah blah

--- newAlphabet/newAlphabet.cpp
+++ newAlphabet/newAlphabet.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <string>
+
+char translate(unsigned int num) {
+    int pos;
+    for (auto i = 0; i < 26; i++) {
+        if (num & (1 << i)) {
+            pos = i;
+            break;
+        }
+    }
+
+    char initial;
+    int is_upper = (num >> 26) & 1;
+    if (is_upper == 1) {
+        initial = 'A';
+    } else if (is_upper == 0) {
+        initial = 'a';
+    }
+    return (initial + pos);
+}
+
 int main(int argc, char *argv[]) {
-    
+    std::string word;
+    for (auto i = 1; i < argc; i++) {
+        unsigned int num = std::stoul(argv[i]);
+        word += translate(num);
+    }
+    std::cout << "You entered the word: " << word << std::endl;
 }
