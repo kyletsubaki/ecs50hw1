3c05841f45fbff255d92fcb5eab89e2176379441
ThePotatoArchivist <josiahfu@gmail.com>
2025-10-06 10:21:21 -0700

    Fix semicolons

--- newAlphabet/newAlphabet.cpp
+++ newAlphabet/newAlphabet.cpp
@@ -2,29 +2,31 @@
 #include <string>
 
 char translate(unsigned int num) {
-    int pos;
-    for (auto i = 0; i < 26; i++) {
+    int pos;
+    for (auto i = 0; i < 26; i++) {
         if (num & (1 << i)) {
-            pos = i;
-            break;
+            pos = i;
+            break;
         }
     }
 
-    char initial;
-    int is_upper = (num >> 26) & 1;
+    char initial;
+    int is_upper = (num >> 26) & 1;
     if (is_upper == 1) {
-        initial = 'A';
+        initial = 'A';
     } else if (is_upper == 0) {
-        initial = 'a';
+        initial = 'a';
     }
-    return (initial + pos);
+    return (initial + pos);
 }
 
 int main(int argc, char *argv[]) {
-    std::string word;
-    for (auto i = 1; i < argc; i++) {
-        unsigned int num = std::stoul(argv[i]);
-        word += translate(num);
+    std::string word;
+    for (auto i = 1; i < argc; i++) {
+        unsigned int num = std::stoul(argv[i]);
+        word += translate(num);
     }
-    std::cout << "You entered the word: " << word << std::endl;
+    std::cout << "You entered the word: " << word << std::endl;
 }
+
+// You probably shouldn't have your homework on a public repo
